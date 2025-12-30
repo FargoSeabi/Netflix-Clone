89de48ef66323eeec2c36c55fbe7303ee0cf7105
MOSES SEABI <160843085+FargoSeabi@users.noreply.github.com>
2025-12-31 00:43:47 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,7 @@
+const sign = document.getElementById('sign-in');
+function myname(){
+    const name = prompt("Enter your name");
+    alert("Welcome to Netflix "+name);
+}
+myname();
+alert("Welcome To Netflix Sa!!");
